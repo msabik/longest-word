be7129d8a66d420c25ba77151320bb2b43029104
msabik <martinsabik@gmail.com>
2019-04-02 14:39:36 +0200

    __init__

---
+++ game.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        grid_length = 9
+        letters = string.ascii_uppercase
+        for i in range(grid_length):
+            self.grid.append(random.choice(letters))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy()  # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
